499a904703a300566713ea0579124eb2db0cb9b2
Anshul Abrol <abroa01@pfw.edu>
2024-11-01 14:11:13 -0400

    Updated the User-Agent in headers and Modified Logging

--- src/MIEApi.js
+++ src/MIEApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import logger from './logger.js'; 
+import logger from './logger.js';
 import { endpoints } from './apiConfig.js';
+import { version } from "../package.json";
 
 class MIEApi {
   static sessionCache = new Map();
@@ -11,56 +12,67 @@ class MIEApi {
     this.userId = userId;
     this.ip = ip;
     this.isRefreshingSession = false;
+    logger.info(`MIEApi instance created for userId: ${this.userId}`, { baseUrl: this.baseUrl });
   }
 
   static getSessionKey(baseUrl, userId) {
-    return `${baseUrl}_${userId}`;
+    const sessionKey = `${baseUrl}_${userId}`;
+    logger.debug(`Generated session key: ${sessionKey}`);
+    return sessionKey;
   }
 
   async refreshSession() {
-    if (this.isRefreshingSession) return;
+    if (this.isRefreshingSession) {
+      logger.info(`Session refresh already in progress for userId: ${this.userId}`);
+      return;
+    }
     this.isRefreshingSession = true;
+    logger.info(`Starting session refresh for userId: ${this.userId}`);
 
     try {
       const refreshUrl = `${this.baseUrl}?f=layoutnouser&name=BlueHive_Refresh_Session&user_id=${encodeURIComponent(this.userId)}&connectToken=${encodeURIComponent(this.connectToken)}&ip_address=${encodeURIComponent(this.ip)}&raw&json`;
 
-      logger.info(`Refreshing connection to: ${refreshUrl}, 'for userId:', ${this.userId}`);
-
+      logger.debug(`Attempting to refresh session with URL: ${refreshUrl}`);
       const response = await axios.get(refreshUrl, {
         headers: {
-          'User-Agent': 'BlueHive AI (Refresh Connection)',
+          'User-Agent': `mieapi (Refresh Connection)/${version}`
         }
       });
 
       if (!response || response.data.status !== 200) {
+        logger.warn(`Session refresh failed with status: ${response?.data?.status || 'unknown'}`);
         throw new Error(response?.data?.message || 'Error connecting to WebChart.');
       }
 
       const getCookieResponse = await axios.get(`${this.baseUrl}?f=wcrelease&json`, {
         headers: {
-          'User-Agent': 'BlueHive AI (Get x-db_name)',
+          'User-Agent': `mieapi (Get x-db_name)/${version}`,
         }
       });
 
       const dbName = getCookieResponse.headers['x-db_name'];
-      if (!dbName) throw new Error('DB name not found in response.');
+      if (!dbName) {
+        logger.error('Failed to retrieve DB name from response headers');
+        throw new Error('DB name not found in response.');
+      }
 
       const sessionCookie = `${dbName}_session_id=${this.connectToken}`;
-
       const sessionKey = MIEApi.getSessionKey(this.baseUrl, this.userId);
+
       MIEApi.sessionCache.set(sessionKey, {
         sessionCookie,
         connectTokenRefreshedAt: new Date(),
-        expiration: new Date(new Date().getTime() + 5 * 60 * 1000),
+        expiration: new Date(Date.now() + 5 * 60 * 1000),
       });
 
       this.sessionCookie = sessionCookie;
-      logger.info('Session refreshed for userId:', this.userId);
+      logger.info(`Session successfully refreshed for userId: ${this.userId}`);
     } catch (error) {
-      logger.error('Failed to refresh session:', error);
+      logger.error(`Error during session refresh for userId: ${this.userId}:`, { error });
       throw error;
     } finally {
       this.isRefreshingSession = false;
+      logger.info(`Session refresh completed for userId: ${this.userId}`);
     }
   }
 
@@ -69,30 +81,31 @@ class MIEApi {
     const cachedSession = MIEApi.sessionCache.get(sessionKey);
 
     if (cachedSession && cachedSession.sessionCookie) {
-      const now = new Date();
-      const timeElapsed = now - new Date(cachedSession.connectTokenRefreshedAt);
+      const now = Date.now();
+      const timeElapsed = now - new Date(cachedSession.connectTokenRefreshedAt).getTime();
       if (timeElapsed < 5 * 60 * 1000) {
-        logger.info('Using cached session for userId:', this.userId);
+        logger.info(`Using valid cached session for userId: ${this.userId}`);
         this.sessionCookie = cachedSession.sessionCookie;
         return;
       } else {
-        logger.info('Cached session expired for userId:', this.userId);
+        logger.info(`Cached session expired for userId: ${this.userId}, refreshing`);
         MIEApi.sessionCache.delete(sessionKey);
       }
+    } else {
+      logger.info(`No cached session available for userId: ${this.userId}, creating a new one`);
     }
 
-    logger.info('No valid session found, refreshing session for userId:', this.userId);
     await this.refreshSession();
   }
 
   async request(endpoint, params = {}, body = null, method = 'GET') {
-    await this.ensureSession();
+    logger.info(`Initiating ${method} request to endpoint: ${endpoint}`, { userId: this.userId, params });
 
+    await this.ensureSession();
     const apiEndpoint = this.getEndpoint(endpoint) || endpoint;
-    const queryString = params.filter+params.limit;
+    const queryString = params.filter + params.limit;
     const b64Endpoint = Buffer.from(`${method}/${apiEndpoint}/${queryString}`).toString('base64');
     const url = `${this.baseUrl}/json/${b64Endpoint}`;
-    console.log(url);
 
     try {
       const response = await axios({
@@ -100,39 +113,44 @@ class MIEApi {
         url,
         headers: {
           'Content-Type': method === 'GET' ? 'application/x-www-form-urlencoded' : 'application/json',
-          Cookie: this.sessionCookie
+          Cookie: this.sessionCookie,
         },
-        data: body
+        data: body,
       });
-      console.log(response);
+
       if (!response.data || (response.data.meta && response.data.meta.status !== '200')) {
+        logger.warn(`Request failed at endpoint: ${endpoint}, Status: ${response.data?.meta?.status}`);
         throw new Error(response.data?.meta?.message || 'API request failed');
       }
 
+      logger.info(`Request successful for endpoint: ${endpoint}`, { userId: this.userId, status: response.data.meta.status });
       return response.data;
     } catch (error) {
-      logger.error(`${method} error:`, error);
+      logger.error(`Error in ${method} request to endpoint: ${endpoint} for userId: ${this.userId}`, { error });
       throw error;
     }
   }
 
-  // Update API methods to handle params correctly
   get(endpoint, params) {
+    logger.info(`GET request to endpoint: ${endpoint}`, { userId: this.userId });
     return this.request(endpoint, params);
   }
 
   post(endpoint, params, data) {
+    logger.info(`POST request to endpoint: ${endpoint}`, { userId: this.userId });
     return this.request(endpoint, params, data, 'POST');
   }
 
   put(endpoint, params, data) {
+    logger.info(`PUT request to endpoint: ${endpoint}`, { userId: this.userId });
     return this.request(endpoint, params, data, 'PUT');
   }
 
   getEndpoint(endpoint) {
     const apiEndpoint = endpoints[endpoint.toLowerCase()];
+    logger.debug(`Resolved endpoint: ${endpoint} to ${apiEndpoint}`);
     return apiEndpoint || endpoint;
   }
 }
-  
+
 export default MIEApi;
